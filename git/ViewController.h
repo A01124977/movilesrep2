e2a88a9b54b857d56346277467b4df0f9ffd95ee
moviles <moviles@moviless-Mac-Pro-92.local>
2013-04-02 19:21:44 -0600

    Initial Commit

---
+++ git/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  git
+//
+//  Created by moviles on 4/2/13.
+//  Copyright (c) 2013 ITESM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ git/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  git
+//
+//  Created by moviles on 4/2/13.
+//  Copyright (c) 2013 ITESM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
